8b9f7d64c7645dbf5d67e17d418189079afd5a54
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-09-21 20:28:12 +0200

    feat(proxmox): add hashicorp/local provider

--- terraform/proxmox/versions.tf
+++ terraform/proxmox/versions.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = "~> 1.12"
 
   required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
+    }
     proxmox = {
       source  = "bpg/proxmox"
       version = "0.83.2"
