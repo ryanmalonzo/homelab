665226f35592d4c01cea5335a05a60c232cb8e4b
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-02-03 23:22:30 +0100

    feat: convert to flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "chaldea";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.chaldea = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
