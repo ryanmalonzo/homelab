a096f80df7b0c5023e81fe92c7d20569b9606023
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-02-04 22:21:09 +0100

    feat: create pangolin subdomain via terraform

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+locals {
+  root_domain = "chaldea.dev"
+  cname_subdomains = [
+    "pangolin"
+  ]
+
+  dns_records = concat(
+    [
+      {
+        name    = local.root_domain
+        type    = "A"
+        content = var.pangolin_ip
+      }
+    ],
+    [
+      for sub in local.cname_subdomains : {
+        name    = "${sub}.${local.root_domain}"
+        type    = "CNAME"
+        content = local.root_domain
+      }
+    ]
+  )
+}
+
+resource "cloudflare_dns_record" "dns_records" {
+  for_each = { for rec in local.dns_records : rec.name => rec }
+
+  zone_id = var.cloudflare_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  content = each.value.content
+  ttl     = 1
+  proxied = false
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "cloudflare_zone_id" {
+  type    = string
+  default = "763f4223335c61f44d73e6955211152a"
+}
+
+variable "pangolin_ip" {
+  type    = string
+  default = "217.154.121.137"
+}
