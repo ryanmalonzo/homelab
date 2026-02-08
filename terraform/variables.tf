e5875805baa8b386d4c00e2878686b6c9814be3b
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-02-08 18:10:52 +0100

    chore: migrate terraform variables to github

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,11 @@
 variable "cloudflare_zone_id" {
-  type    = string
-  default = "763f4223335c61f44d73e6955211152a"
+  type = string
 }
 
 variable "pangolin_ip" {
-  type    = string
-  default = "217.154.121.137"
+  type = string
 }
 
 variable "tailscale_ip" {
-  type        = string
-  default     = "100.93.61.51"
+  type = string
 }
