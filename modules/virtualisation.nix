fd26100bf42e3962e1b419d6e1d99b24dfb0eb5d
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-02-09 08:51:23 +0100

    chore(podman): prune images weekly (#19)

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -3,7 +3,11 @@
 {
   virtualisation.podman = {
     enable = true;
-    # Create the default bridge network for podman
     defaultNetwork.settings.dns_enabled = true;
+    autoPrune = {
+      enable = true;
+      dates = "weekly";
+      flags = [ "--all" ];
+    };
   };
 }
