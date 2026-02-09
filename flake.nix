965b5bf33395fc5156b817aec5842638c6c4291f
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-02-09 07:33:32 +0100

    chore(nix): use determinate nix (#16)
    * chore(nix): use determinate nix
    
    * chore(ci): only run steps when files changed

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,6 +14,7 @@
     {
       self,
       nixpkgs,
+      determinate,
       sops-nix,
     }:
     {
@@ -24,6 +26,7 @@
       nixosConfigurations.chaldea = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          determinate.nixosModules.default
           sops-nix.nixosModules.sops
           ./configuration.nix
           ./modules/networking.nix
