ae665164c866dcab1cdd14c9d2c3050edf95f2c8
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-07-22 22:40:49 +0200

    feat(terraform): import cloudflare dns records
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+locals {
+  root_domain = "chaldea.dev"
+  # cname_subdomains = [
+  #   "pangolin",
+  #   "pdf",
+  #   "music",
+  #   "photos"
+  # ]
+
+  internal_dns_records = [
+    {
+      name    = "*.internal.${local.root_domain}"
+      type    = "A"
+      content = var.tailscale_ip
+    }
+  ]
+
+  dns_records = concat(
+    # [
+    #   {
+    #     name    = local.root_domain
+    #     type    = "A"
+    #     content = var.pangolin_ip
+    #   }
+    # ],
+    # [
+    #   for sub in local.cname_subdomains : {
+    #     name    = "${sub}.${local.root_domain}"
+    #     type    = "CNAME"
+    #     content = local.root_domain
+    #   }
+    # ],
+    local.internal_dns_records
+  )
+}
+
+resource "cloudflare_dns_record" "dns_records" {
+  for_each = { for rec in local.dns_records : rec.name => rec }
+
+  zone_id = var.cloudflare_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  content = each.value.content
+  ttl     = 1
+  proxied = false
+}
+
---
+++ terraform/versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "~> 1.12"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "5.22.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "chaldea-terraform-state"
+    key    = "terraform.tfstate"
+    region = "eu-central-003"
+    endpoints = {
+      s3 = "https://s3.eu-central-003.backblazeb2.com"
+    }
+
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
